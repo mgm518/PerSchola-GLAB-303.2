ec6fe712cfa96c42314ee946781d06d7a87f3f08
mgm518 <mgm518@yahoo.com>
2024-03-15 15:48:00 -0700

    * Added PA 303.2.3 * Updated README.md

--- GLAB-303.2/src/com/perscholas/java_basics/JavaBasics.java
+++ GLAB-303.2/src/com/perscholas/java_basics/JavaBasics.java
@@ -1,16 +1,21 @@
 package com.perscholas.java_basics;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class JavaBasics {
 
     private static final double PI = 3.141592653589793238462643383279502884197;
     public static void main(String[] args) {
-//        PA303_2_1();
-        PA303_2_2();
+        PA_303_2_1();
+        PA_303_2_2();
+        PA_303_2_3();
     }
 
-    private static void PA303_2_1() {
+    private static void PA_303_2_1() {
+        System.out.println("**************************************************\n"
+                +"PA 303.2.1 - Core Java - Variables\n"
+                +"**************************************************");
 //● Write a program that declares two integer variables, assigns an integer to each, and
 //        adds them together. Assign the sum to a variable. Print out the result.
         int int1 = 3, int2 = 5, intSum = int1+int2;
@@ -63,17 +68,23 @@ public class JavaBasics {
 
         double subtotal = item1.price*item1.count + item2.price*item2.count + item3.price*item3.count;
         double totalSale = subtotal*(1+SALES_TAX);
-        System.out.printf("The final sales is $%1.2f%n", totalSale);
+        System.out.printf("The final sales is $%1.2f%n%n", totalSale);
     }
 
-    private static void PA303_2_2() {
+    private static void PA_303_2_2() {
+        System.out.println("**************************************************\n"
+                +"PA 303.2.2 - Core Java - Scanner Class\n"
+                +"**************************************************");
         // This section is completed on HackerRank.com.  I'll be pasting the code I submit there to here
         //  so that I can have a reference.
 
         // ********************
         // java Stdin and Stdout I
         // ********************
-        Scanner scan = new Scanner(System.in);
+        String fixedInput = "42\n100\n125\n42\n3.1415\nWelcome to HackerRank's Java tutorials!\n";
+        Scanner scan =
+//                new Scanner(System.in);
+                new Scanner(fixedInput);
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
@@ -95,6 +106,94 @@ public class JavaBasics {
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
         scan.close();
+        System.out.println();
+    }
+
+    private static void PA_303_2_3() {
+        System.out.println("**************************************************\n"
+                +"PA 303.2.3 - Operators and Numbers\n"
+                +"**************************************************");
+
+//        1. Write a program that declares an integer a variable x, assigns the value 2 to it, and
+//        prints out the binary string version of the number ( Integer.toBinaryString(x) ). Now,
+//        use the left shift operator (<<) to shift by 1 and assign the result to x. Before printing
+//        the results, write a comment with the predicted decimal value and binary string. Now,
+//        print out x in decimal form and in binary notation.
+//        Perform the preceding exercise with the following values:
+//                    a. 9
+//                    b. 17
+//                    c. 88
+        System.out.print("1. ");
+        int[] xPart1 = {2, 9, 17, 88};
+        for (int i=0; i < xPart1.length; i++){
+            System.out.print("\tx: "+Integer.toBinaryString(xPart1[i])+"; x <<= 1 = ");
+            xPart1[i] <<= 1; // x == {4, 18, 34, 176}
+            System.out.println(Integer.toBinaryString(xPart1[i])+" ("+xPart1[i]+")");
+        }
+//        2. Write a program that declares a variable x, and assigns 150 to it, and prints out the
+//        binary string version of the number. Now use the right shift operator (>>) to shift by 2
+//        and assign the result to x. Write a comment indicating what you anticipate the
+//        decimal and binary values to be. Now print the value of x and the binary string.
+//        Perform the preceding exercise with the following values:
+//                    a. 225
+//                    b. 1555
+//                    c. 32456
+        System.out.print("\n2. ");
+        int[] xPart2 = {150, 225, 1555, 32456};
+        // the binary values: { 1001 0110,
+        //                      1110 0001,
+        //                 0110 0001 0011,
+        //            0111 1110 1100 1000 }
+        for (int i=0; i < xPart2.length; i++){
+            System.out.print("\tx: "+Integer.toBinaryString(xPart2[i])+"; x >>= 2 = ");
+            xPart2[i] >>= 2;
+            // the new binary values: {   10 0101,
+            //                            11 1000,
+            //                       01 1000 0100,
+            //                  01 1111 1011 0010 }
+            System.out.println(Integer.toBinaryString(xPart2[i])+" ("+xPart2[i]+")");
+        }
+//        3. Write a program that declares three int variables: x, y, and z. Assign 7 to x and 17 to y.
+//        Write a comment that indicates what you predict will be the result of the bitwise and
+//        operation on x and y. Now use the bitwise AND (&) operator to derive the decimal and
+//        binary values, and assign the result to z.
+        System.out.print("\n3. ");
+        int x=7 /*0111*/, y=17 /*1 0001*/, z= x & y; /* 0 0001 */
+        System.out.printf("x & y = z: %d (%s)", z, Integer.toBinaryString(z));
+//        4. Now, with the preceding values, use the bitwise and operator to calculate the “or”
+//        value between x and y. As before, write a comment that indicates what you predict
+//        the values to be before printing them out.
+        System.out.print("\n4. ");
+        z= x | y; /* 1 0111 */
+        System.out.printf("x | y = z: %d (%s)", z, Integer.toBinaryString(z) );
+//        5. Write a program that declares an integer variable, assigns a number, and uses a
+//        postfix increment operator to increase the value. Print the value before and after the
+//        increment operator.
+        System.out.print("\n5. ");
+        int step = 2;
+        System.out.print("\tStep started as '"+(step++)+"' and is now '"+step+"'");
+//        6. Write a program that demonstrates at least three ways to increment a variable by 1
+//        and does this multiple times. Assign a value to an integer variable, print it, increment
+//        by 1, print it again, increment by 1, and then print it again.
+        System.out.print("\n6. ");
+        int moreSteps = new Random().nextInt(1000);
+        System.out.println("\tNew using :'"+moreSteps+"'");
+        System.out.println("\t\tAddition assignment:\tx+=1\t-> "+(moreSteps+=1));
+        System.out.println("\t\tIncrement (pre/post):\t++x \t-> "+(++moreSteps));
+        moreSteps = moreSteps + 1;
+        System.out.println("\t\tBasic self-assignment:\tx=x+1\t-> "+moreSteps);
+//        7. Write a program that declares two integer variables: x, and y, and then assigns 5 to x
+//        and 8 to y. Create another variable sum and assign the value of ++x added to y, and
+//        print the result. Notice the value of the sum (should be 14). Now change the
+//        increment operator to postfix (x++) and re-run the program. Notice what the value of
+//        the sum is. The first configuration incremented x, and then calculated the sum, while
+//        the second configuration calculated the sum, and then incremented x.
+        System.out.print("\n7. ");
+        x = 5; y = 8;
+        int sum = ++x + y;
+        System.out.println("\tsum = ++x + y = '"+sum+"' (14, matches: "+(sum==14)+")");
+        sum = x++ + y;
+        System.out.println("\tsum = x++ + y = '"+sum+"' (14, matches: "+(sum==14)+")");
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Lesson 303.2 - Java Programming Fundamentals
+# Lesson 303.2 - Java Programming Fundamentals
+
+This consists of the practice assignments 303.2.1 - Variables, 303.2.2 - Scanner Class, and 303.2.3 - Operators and Numbers.
+
+~ 303.2.2 does request submitting a link to one's HackerRank profile, which I did.  For convenience, I added screenshots of the code submissions and their results in this repo.  The project also has copy of the code used for the submission in the project.
+
+* HackerRank Profile: [mmcguire7432](https://www.hackerrank.com/profile/mmcguire7432)
+
+* Java Stdin and Stdout I
+![Java Stdin and Stdout I](PA%20303.2.2%20-%20Java%20Stdin%20and%20Stdout%20I%20-%202024-03-15%2014%2018%2009.png)
+* Java Stdin and Stdout II
+![Java Stdin and Stdout II](PA%20303.2.2%20-%20Java%20Stdin%20and%20Stdout%20II%20-%202024-03-15%2014%2021%2040.png)
